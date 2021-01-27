5a1a943a7b82edf2b97dbfa0597a56800b87b05b
Moisem <al221910022@gmail.com>
2021-01-27 10:49:42 -0600

    Pantalla detalles

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import React from 'react';
+import CoinsStack from './src/componentes/coins/CoinsStack';
+import { NavigationContainer} from '@react-navigation/native';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <CoinsStack />
+      </NavigationContainer>
+    </>
   );
 }
-
 export default App;
